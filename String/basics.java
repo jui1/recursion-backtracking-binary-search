86c19a7c54b7b58c6654b7c1ced6723ac97cc524
Mandal <jui.mandal@capgemini.com>
2025-02-17 15:40:36 +0530

    Your commit message

---
+++ String/basics.java
@@ -0,0 +1,13 @@
+package String;
+
+public class basics {
+    public static void main(String[] args) {
+        String a = "kunal";
+        String b = "kunal";
+
+        System.out.println(a==b);
+
+        System.out.println(a.equals(b));
+    }
+    
+}
